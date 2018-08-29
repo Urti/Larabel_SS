1a266daaafeeb4af6daa34d2ea6cc3e8cbdc40e9
Piotr Ostaniewicz <piotr.ostaniewicz@signify.com>
2018-08-29 20:23:48 +0200

    Seeder and faker

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,40 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $faker = Faker::create('pl_PL');
+        /*=====Zmienne=====*/
+        $number_of_users = 20;
+        $password = 'pass';
+        /*=================*/
+        for ($i=1; $i < $number_of_users; $i++) { 
+
+            if ($i === 1) {
+                DB::table('users')->insert([
+                    'name' => 'Piotr Ostaniewicz',
+                    'email' => 'piotr-ostaniewicz@wp.pl',
+                    'sex' => 'm',
+                    'password' => bcrypt($password),
+                ]);
+            }
+            
+            $sex = $faker->randomElement(['m','f']);
+
+            switch ($sex) {
+                case 'm':
+                    $name = $faker->firstNameMale . ' ' . $faker->lastNameMale;
+                    break;
+                case 'f':
+                $name = $faker->firstNameFemale . ' ' . $faker->lastNameFemale;
+                    break;
+                
+            }
+
+            DB::table('users')->insert([
+                'name' => $name,
+                'email' => str_replace('-', '', str_slug($name)) . '@' . $faker->safeEmailDomain,
+                'sex' => $sex,
+                'password' => bcrypt($password),
+            ]);
+        }
     }
 }
